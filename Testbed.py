b8b969154f4de0371e64c500184647936c192f9b
mcastill1234 <30333120+mcastill1234@users.noreply.github.com>
2017-08-14 08:03:06 -0400

    Added testbed

---
+++ Testbed.py
@@ -0,0 +1,18 @@
+import string
+
+shift_dict = {}
+dkeysl = string.ascii_lowercase
+dkeysu = string.ascii_uppercase
+
+shift = 2
+
+
+if
+for char in dkeysl:
+    shift_dict[char] = dke
+
+char = 'c'
+shift = 2
+
+temp = dkeysl[dkeysl.index(char)+shift]
+print(temp)
